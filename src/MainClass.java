ebb02c09264e28bece6a765de115ebd7dedbbffb
R3ZAR <rrahimi25@gmail.com>
2022-12-11 10:41:11 +0330

    second

---
+++ src/MainClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter txt");
+        String txt = scn.next();
+
+        StringBuilder result =  new StringBuilder();
+
+        for (int i = (txt.length()-1); i >= 0; i--){
+
+           result = result.append(txt.charAt(i));
+
+        }
+
+        System.out.println(result);
+        System.out.println("gitt");
+
+    }
+
+}
