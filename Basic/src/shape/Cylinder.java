66351f9b5ae0c85c870b751ab34be18619ffc0ac
r3zar <rrahimi25@gmail.com>
2023-03-29 14:12:45 +0430

    java basic exam

---
+++ Basic/src/shape/Cylinder.java
@@ -0,0 +1,26 @@
+package src.shape;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+
+    public double getVolume(){
+
+
+        return super.getArea() * height;
+
+
+    }
+
+
+}
